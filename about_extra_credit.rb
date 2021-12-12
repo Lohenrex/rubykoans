33c2aa4eecd367d29d213a4913b2df94b2a820d4
Alejandro Rebollar <lohenrex.ragnarok@gmail.com>
2021-12-11 20:01:26 -0600

    Koans completed except for extra_credit

--- about_extra_credit.rb
+++ about_extra_credit.rb
@@ -6,3 +6,27 @@
 # You already have a DiceSet class and score function you can use.
 # Write a player class and a Game class to complete the project.  This
 # is a free form assignment, so approach it however you desire.
+
+
+module GreedGame
+	attr_reader :non_scoring_dice
+	@non_scoring_dice = [2, 3, 4, 6]
+
+	class Player
+		def initialize(player_name)
+			@player_name = player_name
+			@score = 0
+		end
+
+		def can_accumulate?
+			@score >= 300		
+		end
+	end
+	
+	class Game
+		def initialize
+			@player_ranking = []
+			@current_turn = 0
+		end
+	end
+end
